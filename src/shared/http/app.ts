651f6549125e73554b421523369e650cdcb689b5
Uesli Almeida <almeida.uesli@gmail.com>
2026-08-19 20:01:18 -0300

    feat(logger): middleware config

--- src/shared/http/app.ts
+++ src/shared/http/app.ts
@@ -1,9 +1,14 @@
+import logger from '@config/logger';
 import express from 'express';
-
-// import logger from '#config/logger';
+import helmet from 'helmet';
+import morgan from 'morgan';
 
 const app = express();
 
+// Middlewares
+app.use(helmet()); // Security headers
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('combined', { stream: { write: (message) => logger.info(message.trim()) } })); // HTTP request logging
 
 export default app;
